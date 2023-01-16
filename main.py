5c1a6efd02258e9ea9238d51e75c00c4396475b7
mtndrew <63341389+devmtndrew@users.noreply.github.com>
2023-01-16 14:38:26 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import interactions 
+import os
+
+token = os.environ.get("TOKEN")
+
+bot = interactions.Client(token=token)
+
+#your server id here server_id = 
+
+@bot.event
+async def on_ready():
+  print("Bot is ready!")
+
+@bot.command(
+  name="hello",
+  description="Says hello!",
+  scope=server_id
+)
+async def hello(ctx: interactions.CommandContext):
+  await ctx.send(f"Hello, {ctx.author.mention}")
+
+bot.start()
